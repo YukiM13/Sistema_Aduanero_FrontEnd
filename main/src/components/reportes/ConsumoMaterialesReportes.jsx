d57ff0cd058bb90a6087fffd02c4d2d9d2c5e2bc
EmpanadaEncanelada <mariomoya828@gmail.com>
2025-05-15 13:43:06 -0600

    Update ConsumoMaterialesReportes.jsx

--- main/src/components/reportes/ConsumoMaterialesReportes.jsx
+++ main/src/components/reportes/ConsumoMaterialesReportes.jsx
@@ -36,30 +36,34 @@ import declaracionvalor from '../../models/devaspendientesModel.js'
 
 const ConsumoMaterialesReporte = ()=>{
 
-    var datos = {fecha_inicio: new Date(), fecha_fin: new Date()};
-    var fechaInicio ;
-    var fechaFin ;
+    const [datos, setDatos] = useState({fecha_inicio: new Date(), fecha_fin: new Date()}) ;
+    const [fechaInicio, setFechaInicio] = useState(new Date());
+    const [fechaFin, setFechaFin] = useState(new Date());
+    
 
     const apiUrl = process.env.REACT_APP_API_URL;
     const apiKey = process.env.REACT_APP_API_KEY;
 
     const [consumos, setConsumos] = useState([]);
 
-    const CargarConsumos = ()=>{
+    const CargarConsumos =()=>{
 
-        datos.fecha_inicio = fechaInicio;
-        datos.fecha_fin = fechaInicio;
+      //setDatos({fecha_inicio: fechaInicio, fecha_fin: fechaFin});
+      // datos.fecha_inicio = fechaInicio;
+      //   datos.fecha_fin = fechaInicio;
+      const parametros = {fecha_inicio: fechaInicio, fecha_fin: fechaFin};
+      console.log("datosdatos", parametros);
 
-        axios.get(`${apiUrl}/api/Reportes/Consumo_Materiales`, datos,{
+        axios.post(`${apiUrl}/api/Reportes/Consumo_Materiales`, parametros,{
       headers: { 'XApiKey': apiKey }
     })
-    .then(data =>{ setConsumos(data);
+    .then(Response =>{ setConsumos(Response.data.data);
 
-        console.log("cargados?", data);
-        alert();
+        console.log("cargados?", Response.data);
+        // alert("??");
 
     })
-    .catch(error => console.error('Error al obtener las Ducas:', error));
+    .catch(error => console.error('Error al obtener consumos:', error));
 
     }
 
@@ -77,7 +81,7 @@ const ConsumoMaterialesReporte = ()=>{
                 <ParentCard>
                     <Grid container spacing={3} mb={3}>
 
-                  <Grid item lg={6} md={12} sm={12}>
+                  <Grid item lg={4} md={4} sm={4}>
                     <CustomFormLabel>Fecha Inicio</CustomFormLabel>
                     <CustomTextField
                         fullWidth
@@ -85,13 +89,14 @@ const ConsumoMaterialesReporte = ()=>{
                         id="fechaInicio"
                         type="date"
                         value={fechaInicio}
+                        onChange={e => setFechaInicio(e.target.value)}
                         //onChange={formik.handleChange}
                         //error={formik.touched.fechaInicio && Boolean(formik.errors.fechaInicio)}
                         //helperText={formik.touched.fechaInicio && formik.errors.fechaInicio}
                     />
                   </Grid>
 
-                     <Grid item lg={6} md={12} sm={12}>
+                     <Grid item lg={4} md={4} sm={4}>
                     <CustomFormLabel>Fecha Fin</CustomFormLabel>
                     <CustomTextField
                         fullWidth
@@ -99,18 +104,24 @@ const ConsumoMaterialesReporte = ()=>{
                         id="fechaFin"
                         type="date"
                         value={fechaFin}
+                        onChange={e => setFechaFin(e.target.value)}
                         // onChange={formik.handleChange}
                         // error={formik.touched.fechaFin && Boolean(formik.errors.fechaFin)}
                         // helperText={formik.touched.fechaFin && formik.errors.fechaFin}
                     />
                   </Grid>
 
-                   <Grid item>
-                    <Button variant="contained" onClick={CargarConsumos()} startIcon={<Search />}>
+                   <Grid item lg={4} md={4} sm={4}>
+                    <CustomFormLabel><br/></CustomFormLabel>
+                    <Button style={{width: "100%", height: "43%"}} variant="contained" onClick={CargarConsumos} startIcon={<Search />}>
                       Buscar
                     </Button>
                   </Grid>
         </Grid>
+
+        <hr />
+        <h4 className='text-center'>Vista Previa</h4>
+        <hr />
         <Table>
           <TableHead>
             <TableRow>
@@ -121,7 +132,8 @@ const ConsumoMaterialesReporte = ()=>{
           <TableBody>
             {consumos.map((item) => (
               <TableRow key={item.mate_Descripcion}>
-                <TableCell>{item.TotalMaterial}</TableCell>
+                <TableCell>{item.mate_Descripcion}</TableCell>
+                <TableCell>{item.totalMaterial}</TableCell>
 
               </TableRow>
             ))}
